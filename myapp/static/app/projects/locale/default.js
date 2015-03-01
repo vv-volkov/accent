a3dbf3f9d72b4415806fe1bfa3e17e7ee416f33d
Viktor Volkov <vv_volkov@mail.ru>
2015-03-01 13:36:32 -0500

    update

--- myapp/static/app/projects/locale/default.js
+++ myapp/static/app/projects/locale/default.js
@@ -11,22 +11,5 @@ var Loc = {
     "addsubobject"    :   "Add subobject",
     "addclone"        :   "Add clone",
     "moveup"          :   "Move up",
-    "movedown"        :   "Move down",
-    "Db.Users"        :   "Users",
-    "Db.History"      :   "History",
-    "Db.User"         :   "User",
-    "Db.Operation"    :   "Operation",
-    "Db.IP"           :   "IP",
-    "Db.Name"         :   "Name",
-    "Db.Field"        :   "Field",
-    "Db.Type"         :   "Type",
-    "Db.Time"         :   "Time",
-    "Db.BeforeChange" :   "Before Change",
-    "Db.AfterChange"  :   "After Change",
-    "Db.String"       :   "String",
-    "Db.Text"         :   "Text",
-    "Db.Settings"     :   "Settings",
-    "Db.ObjectEditor" :   "Object Editor",
-    "Db.Password"     :   "Password",
-    "Db.Login"        :   "Login"
+    "movedown"        :   "Move down"
 }
--- myapp/static/app/projects/modules/objectEditor/setup.py
+++ myapp/static/app/projects/modules/objectEditor/setup.py
@@ -4,43 +4,131 @@ System setup
 """
 import json
 import MySQLdb
-from pprint import pprint
+from pymongo import MongoClient
+import psycopg2
+from bson.objectid import ObjectId
 
 def init():
     """
-    Read configuration files and make initial dependencies
+    Read configuration files and make initial setup
     """
     Init = json.load(open('init.json','r'))
     Data = json.load(open('data.json','r'))
     Mapping  = json.load(open('mappings.json','r'))
-    
+    Loc = json.load(open('./locale/lang-'+Init['locale']+'.json','r'))
     for table in Init['tables']:
         if table['name'] in Data['create']:
-            createBefore = Data['create'][table['name']]
-            createResult = dict()
-            mapAttr = Mapping[table['id_db']]
-            for attr in createBefore:
-                if attr in createBefore:
-                    createResult[attr] = mapAttr[createBefore[attr]]  
-            if(len(createResult)):
-                createTable(table['id_db'],table['name'],createResult)
+            commit('create',table['name'],table['id_db'],Init,Data,Mapping,Loc)
+        if table['name'] in Data['insert']:
+            commit('insert',table['name'],table['id_db'],Init,Data,Mapping,Loc)    
 
-def createTable(id_db,tableName,tableDict):
+def commit(action,tableName,id_db,Init,Data,Mapping,Loc):
     """
-    Create a new table or collection     
+    Trigger creation or insertion of data
     """
-    Init = json.load(open('init.json','r'))
+    before = Data[action][tableName]
+    result = dict()
+    mapAttr = Mapping[id_db]
+    if action == 'create':
+        for attr in before:
+            if before[attr] in mapAttr: 
+                result[attr] = mapAttr[before[attr]] 
+        createInsert('create',tableName,id_db,result,Init)
+    elif action == 'insert':
+        for attr in before:
+            for v in attr:
+                if attr[v] in mapAttr:
+                    result[v] = mapAttr[attr[v]]
+                elif attr[v] in Loc:
+                    result[v] = Loc[attr[v]]
+                else:
+                    result[v] = attr[v]
+            createInsert('insert',tableName,id_db,result,Init)  
+            
+def createInsert(action,tableName,id_db,tableDict,Init):
+    """
+    Commit create or insert statement
+    """
     if id_db == "0":
-        for dbs in Init['databases']:
-            if dbs['id'] == "0":
-                db = MySQLdb.connect(dbs['ip'],dbs['user'],dbs['password'],dbs['name'])
-                cursor = db.cursor()     
-                createStr = ""
-                for attr in tableDict:
-                    createStr += "," + attr + " " + tableDict[attr]
-                createStr = "CREATE TABLE " + tableName + "(" +createStr[1:] + ")"
-                cursor.execute(createStr)
-        
+        db = connect(id_db,Init)
+        cursor = db.cursor()
+        sql = prepareSql(action,tableName,tableDict)
+        if action == 'create':
+            cursor.execute(sql)
+        elif action == 'insert':
+            cursor.execute(sql,tuple(tableDict.values()))
+        db.commit() 
+    elif id_db == "1":
+        db = connect(id_db,Init)
+        if action == 'insert':          
+            tableDict['_id'] = ObjectId()
+            db[tableName].insert(tableDict)
+    elif id_db == "3":
+        db = connect(id_db,Init)
+        cursor = db.cursor()
+        autoIncrement = False
+        sql,autoIncrement = preparePostgreSql(action,tableName,tableDict,autoIncrement)
+        if action == 'create':
+            if autoIncrement:
+                cursor.execute("CREATE SEQUENCE auto_id_"+tableName)
+            cursor.execute(sql)  
+        elif action == 'insert':
+            cursor.execute(sql,tuple(tableDict.values()))
+        db.commit()
+
+def preparePostgreSql(action,tableName,tableDict,autoIncrement):
+    """
+    Prepare sql statement for Postgre database
+    """
+    attrs = ""
+    values = ""
+    if action == 'insert':
+        for v in tableDict.keys():
+            attrs += "," + v
+            values += ",%s" 
+        sql = u"INSERT INTO "+tableName+" ("+attrs[1:]+") VALUES("+values[1:]+")"
+    elif action == 'create':
+        for attr in tableDict:
+            if tableDict[attr] == "auto_increment":
+                autoIncrement = True
+                values += "," + attr + " integer NOT NULL DEFAULT nextval('auto_id_"+tableName+"')"
+            else:
+                values += "," + attr + " " + tableDict[attr]
+        sql = "CREATE TABLE " + tableName + "(" +values[1:] + ")"
+    return (sql, autoIncrement)
+    
+    
+def prepareSql(action,tableName,tableDict):
+    """
+    Prepare sql statement
+    """
+    attrs = ""
+    values = ""
+    if action == 'insert':
+        for v in tableDict.keys():
+            attrs += "," + v
+            values += ",%s" 
+        sql = u"INSERT INTO "+tableName+" ("+attrs[1:]+") VALUES("+values[1:]+")"
+    elif action == 'create':        
+        for attr in tableDict:
+            values += "," + attr + " " + tableDict[attr]
+        sql = "CREATE TABLE " + tableName + "(" +values[1:] + ")"
+    return sql
+  
+def connect(id_db,Init):
+    """
+    Connect to MySQL database
+    """
+    for dbs in Init['databases']:
+        if id_db == "0" and dbs['id'] == "0":
+            db = MySQLdb.connect(dbs['ip'],dbs['user'],dbs['password'],dbs['name'])            
+        elif id_db == "1" and dbs['id'] == "1":
+            client = MongoClient(dbs['ip'],int(dbs['port']))
+            db = client[dbs['name']]
+        elif id_db == "3" and dbs['id'] == "3":
+            db = psycopg2.connect(host=dbs['ip'],dbname=dbs['name'],user=dbs['user'],password=dbs['password'])
+    return db
+
 
 if __name__ == "__main__":
     init()
